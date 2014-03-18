e62e61892f2a1259533990f0845b4ef105f0519a
batzen <batzen@gmx.org>
2014-03-18 20:18:35 +0100

    Initial commit

---
+++ NAppTracking.Server/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NAppTracking.Server.Startup))]
+namespace NAppTracking.Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
