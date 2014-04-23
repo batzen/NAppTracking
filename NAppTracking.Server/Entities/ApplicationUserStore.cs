cc9cb788518e51a716efedf870be6c23c84c29d0
batzen <batzen@gmx.org>
2014-04-23 21:05:36 +0200

    - added missing file

---
+++ NAppTracking.Server/Entities/ApplicationUserStore.cs
@@ -0,0 +1,12 @@
+﻿namespace NAppTracking.Server.Entities
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class ApplicationUserStore : UserStore<ApplicationUser>
+    {
+        public ApplicationUserStore(EntitiesContext db)
+            : base(db)
+        {
+        }
+    }
+}
